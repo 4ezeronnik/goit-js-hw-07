85126be3163509896def1278ec48936d0dd27cc6
Andrii Reznichenko <4ezeronnik@gmail.com>
2022-05-05 12:01:35 +0300

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,27 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+console.log(createGalleryMarkup(galleryItems));
+
+function createGalleryMarkup(gallery) {
+    const markup = gallery.map(gallery => {
+        
+    })
+    `
+<div class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      src="small-image.jpg"
+      data-source="large-image.jpg"
+      alt="Image description"
+    />
+  </a>
+</div>
+`
+}
+
+
+
 
 console.log(galleryItems);
+
